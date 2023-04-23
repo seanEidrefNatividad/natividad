9c940552925b9a46756db7d1f38e098cb3dc9e48
seanEidrefNatividad <seaneidrefnatividad@gmail.com>
2023-04-23 12:33:11 +0800

    cleardb config

--- web/album/config/database.php
+++ web/album/config/database.php
@@ -1,38 +1,28 @@
 
 
 <?php
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "album";
-
-$conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
-
-if(!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
-}
-
-// Create connection
-// $conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
-
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
-// }
-// echo "Connected successfully";
-
-
+// local
 // $servername = "localhost";
 // $dbUsername = "root";
 // $dbPassword = "";
 // $dbName = "album";
 
-// // Create connection
-// $conn = new mysqli($servername, $dbUsername, $dbPassword);
+// $conn = mysqli_connect($servername, $dbUsername, $dbPassword, $dbName);
 
-// // Check connection
-// if ($conn->connect_error) {
-//   die("Connection failed: " . $conn->connect_error);
+// if(!$conn) {
+//   die("Connection failed: " . mysqli_connect_error());
 // }
-// echo "Connected successfully";
+
+// remote
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 ?>
